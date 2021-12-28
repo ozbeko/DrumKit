6afd1e55ab653b6b1b496d0876b2daa8cf5f67a3
Ömer Faruk Özbek <omer.ozbek.faruk@gmail.com>
2021-12-28 22:01:38 +0300

    Keyboard events and animation on clicked buttons are implemented.

--- index.js
+++ index.js
@@ -1,50 +1,71 @@
-
+// Button click event listener.
 var buttonNumber = document.querySelectorAll('.drum').length;
 
 for(var i = 0; i < buttonNumber; i++){
   document.querySelectorAll('.drum')[i].addEventListener('click', function(){
-
     var buttonName = this.innerHTML;
-
-    switch (buttonName) {
-      case 'w':
-        var crash = new Audio('sounds/crash.mp3');
-        crash.play();
-        break;
-
-      case 'a':
-        var kickBass = new Audio('sounds/kick-bass.mp3');
-        kickBass.play();
-        break;
-
-      case 's':
-        var snare = new Audio('sounds/snare.mp3');
-        snare.play();
-        break;
-
-      case 'd':
-        var tom1 = new Audio('sounds/tom-1.mp3');
-        tom1.play();
-        break;
-
-      case 'j':
-        var tom2 = new Audio('sounds/tom-2.mp3');
-        tom2.play();
-        break;
-
-      case 'k':
-        var tom3 = new Audio('sounds/tom-3.mp3');
-        tom3.play();
-        break;
-
-      case 'l':
-        var tom4 = new Audio('sounds/tom-4.mp3');
-        tom4.play();
-        break;
-
-      default:
-        console.log(buttonName);
-    }
-
+    playSound(buttonName);
+    buttonAnimation(buttonName);
   });
+}
+
+// Keyboard press event listener.
+document.addEventListener('keydown', function(event){
+  playSound(event.key);
+  buttonAnimation(event.key);
+});
+
+
+// Function that makes the sounds.
+function playSound(key){
+
+  switch (key) {
+    case 'w':
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+
+    case 'a':
+      var kickBass = new Audio('sounds/kick-bass.mp3');
+      kickBass.play();
+      break;
+
+    case 's':
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+
+    case 'd':
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+
+    case 'j':
+      var tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+      break;
+
+    case 'k':
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+
+    case 'l':
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+
+    default:
+      console.log(buttonName);
+  }
+}
+
+// Function that makes animation on clicked button.
+function buttonAnimation(key){
+  var clickedButton = document.querySelector("." + key);
+  clickedButton.classList.add("pressed");
+
+  setTimeout(function(){
+    clickedButton.classList.remove("pressed")
+  }, 100);
 }
